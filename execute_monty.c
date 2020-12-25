960d710c9a47f2cd4fcf7b33f6ccef15c248462b
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-25 19:44:40 +0000

    Fix(get_opcode): remove rule to take into account last char of file

--- execute_monty.c
+++ execute_monty.c
@@ -91,8 +91,9 @@ char *get_opcode(void)
 				if (c == ' ' || c == '\n' || monty_data.file_data[idx + 1] == '\0')
 				{
 					last_idx = idx;
-					if (monty_data.file_data[idx + 1] == '\0')
-						last_idx += 1;
+					/* takes the last char of a file if there is EOF */
+					/* if (monty_data.file_data[idx + 1] == '\0')
+						last_idx += 1; */
 					monty_data.data_index = last_idx;
 					opcode = copy_opcode(firts_idx, last_idx);
 					return (opcode);
